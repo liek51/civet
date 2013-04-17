2af2809976c4588c68b5d19c2b470b321819fd8d
Al Simons <al.simons@jax.org>
2013-04-17 10:12:57 -0400

    Restructure to put main program files in top level for ease of use.

--- single_sample_exome.py
+++ single_sample_exome.py
@@ -2,19 +2,22 @@
 
 #The "main" program of the exome tumor only pipeline.
 
-import sys
-import os
+import os, sys, inspect
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+lib_folder = os.path.join(cmd_folder, 'src')
+if lib_folder not in sys.path:
+     sys.path.insert(0, lib_folder)
+
 import pipeline_parse as PL
 def main():
+    global cmd_folder
     if len(sys.argv) != 3:
         print >> sys.stderr, ('The single sample (tumor only) exome pipeline '
                               'requires two arguments: fastq-end-1, fastq-end-2.')
         sys.exit(1)
 
     # Determine the path to the pipeline...
-    pathname = os.path.dirname(sys.argv[0])
-    fullpath = os.path.abspath(pathname)
-    pipeline = os.path.join(fullpath, '../exome/single_sample_exome.xml')
+    pipeline = os.path.join(cmd_folder, 'exome/single_sample_exome.xml')
     try:
         with open(pipeline) as p:
             pass
