fec4aeeda2113103d38856b15080265cb86528a0
Al Simons <al.simons@jax.org>
2013-05-01 16:19:50 -0400

    look for files in /data, not /hpcdata. Also: make cga/0.0 be the default, and put out the directory name if a failure creating new directory.

--- src/utilities.py
+++ src/utilities.py
@@ -3,11 +3,13 @@
 
 import os
 import errno
+import sys
 
 def make_sure_path_exists(path):
     try:
         os.makedirs(path)
     except OSError as exception:
         if exception.errno != errno.EEXIST:
+            print >> sys.stderr, 'Error while creating directory', path
             raise
 
