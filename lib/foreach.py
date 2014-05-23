cd90863f05c4874d1e631accaab7e4ebf8ad0659
Glen Beane <glen.beane@jax.org>
2014-05-23 09:58:04 -0400

    foreach stuff should have been in last commit

--- lib/foreach.py
+++ lib/foreach.py
@@ -9,22 +9,25 @@ class ForEach():
     validTags = [
         'file',
         'related',
-        'step' ]
+        'step',
+        'id' ]
 
-    def __init__(self, e, pipeline_files, skip_validation=False):
+    def __init__(self, e, pipeline_files):
         self.file = None
         self.relatedFiles = {}
         self.steps = []
         self.pipelineFiles = pipeline_files
-        self.skip_validation = skip_validation
+        self.code = "FE"
         att = e.attrib
+        assert 'id' in att, 'the foreach tag must specify an id'
+        self.id = att['id']
         assert 'dir' in att, 'the foreach tag must specify a dir attribute'
         self.dir = att['dir']
         for child in e:
             t = child.tag
             assert t in ForEach.validTags, 'invalid tag in ForEach:' + t
             if t == 'step':
-                #self.steps.append(Step(child, pipelineFiles, skip_validation))
+                #self.steps.append(Step(child, pipelineFiles))
                 self.steps.append(child)
             elif t == 'file':
                 assert not self.file, 'the foreach tag must contain exactly one file tag.'
@@ -34,9 +37,9 @@ class ForEach():
         assert self.file, 'the foreach tag must contain a file tag'
         assert self.file.id not in self.relatedFiles, "a foreach file's id must not be the same as a related file's id" + self.file.id
 
-    def submit(self, name_prefix, foreach_iteration=None):
-        # NOTE: foreach_iteration is a dummy parameter so that ForEach.submit()
-        # has the same signature as Step.submit()
+    def submit(self, name_prefix):
+
+        import pipeline_parse as PL
 
         # Get file list
         # for file in list
@@ -49,30 +52,53 @@ class ForEach():
         all_files = os.listdir(self.pipelineFiles[self.dir].path)
         for fn in all_files:
             if self.file.pattern.match(fn):
-                print "Matched:", self.file.id, fn
                 iteration += 1
                 cleanups = []
                 PipelineFile(self.file.id, fn, True, False, True, False,
                              self.pipelineFiles, True, None, None, None, None,
                              None, None, self.dir, False, False)
                 cleanups.append(self.file.id)
+
                 for relid in self.relatedFiles:
                     rel = self.relatedFiles[relid]
                     rfn = rel.pattern.sub(rel.replace, fn)
-                    PipelineFile(rel.id, rfn, True, False, rel.inout, False,
+                    if rel.is_input:
+                        #we assume related input files are in the same dir
+                        #as the foreach file it is related to
+                        directory = self.dir
+                    elif rel.indir:
+                        directory = rel.indir
+                    else:
+                        #related file is an output file and indir not specified
+                        #write it to the default output directory
+                        directory = None
+                    PipelineFile(rel.id, rfn, True, False, rel.is_input, False,
                                  self.pipelineFiles, True, None, None, None,
-                                 None, None, None, self.dir, False, False)
+                                 None, None, None, directory, False, False)
                     cleanups.append(rel.id)
-                    print 'Related:', rel.id, rfn
                 PipelineFile.fix_up_files(self.pipelineFiles)
 
+                step_iteration = 0
                 for s in self.steps:
-                    step = Step(s, self.pipelineFiles, self.skip_validation)
-                    print 'foreach submitting step:', step.name
-                    for jid in step.submit(name_prefix, iteration):
+                    step_iteration += 1
+                    step = Step(s, self.pipelineFiles)
+                    prefix = "{0}-{1}_S{2}".format(name_prefix, iteration, step_iteration)
+                    for jid in step.submit(prefix):
                         job_ids.append(jid)
                 for jid in cleanups:
                     del self.pipelineFiles[jid]
+        #enqueue "barrier" job here
+        barrier_job = BatchJob('echo "placeholder job used for synchronizing foreach jobs"',
+                               workdir=PipelineFile.get_output_dir(),
+                               depends_on=job_ids,
+                               name="{0}_barrier".format(name_prefix))
+        try:
+            job_id = PL.job_runner.queue_job(barrier_job)
+        except Exception as e:
+            sys.stderr.write(str(e) + '\n')
+            sys.exit(PL.BATCH_ERROR)
+        PL.all_batch_jobs.append(job_id)
+        PL.foreach_barriers[self.id] = job_id
         return job_ids
 
     def check_files_exist(self):
@@ -103,17 +129,26 @@ class ForEachRelated():
         'id',
         'input',
         'pattern',
-        'replace' ]
+        'replace']
+    optionalAtts = [
+        'indir' ]
+
     def __init__(self, e, related_files, pipeline_files):
         atts = e.attrib
         for a in atts:
-            assert a in ForEachRelated.requiredAtts, 'foreach related tag has unrecognized attribute: ' + a
+            assert a in ForEachRelated.requiredAtts or a in ForEachRelated.optionalAtts, 'foreach related tag has unrecognized attribute: ' + a
         for a in ForEachRelated.requiredAtts:
             assert a in atts, 'foreach related tag is missing required attribute: ' + a
         self.id = atts['id']
         assert self.id not in related_files, 'foreach tag contains duplicate related file id: ' + self.id
         assert self.id not in pipeline_files, "a foreach file's id must not be the same as a pipeline file id: " + self.id
-        self.inout = atts['input']
+        self.is_input = atts['input'].upper() == 'TRUE'
         self.pattern = re.compile(atts['pattern'])
         self.replace = atts['replace']
+
+        if 'indir' in atts:
+            self.indir = atts['indir']
+        else:
+            self.indir = None
+        assert not (self.is_input and self.indir), 'foreach related input file must be in same directory as foreach file: ' + self.id
         related_files[self.id] = self
