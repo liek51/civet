9acf5892d533e04f956ac01f75b05b0e08c0da12
Glen Beane <glen.beane@jax.org>
2016-01-28 14:52:29 -0500

    improve error message for xml parsing errors in foreach.py (no more AssertError)

--- lib/foreach.py
+++ lib/foreach.py
@@ -1,13 +1,15 @@
 # Standard imports
+import xml.etree.ElementTree as ET
 
 # Pipeline components
 from step import *
 from pipeline_file import *
 from job_runner.torque import *
+from civet_exceptions import *
 
 
 
-class ForEach():
+class ForEach(object):
     validTags = [
         'file',
         'related',
@@ -28,22 +30,50 @@ class ForEach():
         self.pipelineFiles = pipeline_files
         self.code = "FE"
         att = e.attrib
-        assert 'id' in att, 'the foreach tag must specify an id'
+
+        if 'id' not in att:
+            msg = "The foreach tag must specify an id:\n\n{}".format(ET.tostring(e))
+            raise ParseError(msg)
+
+        if 'dir' not in att:
+            msg = "The foreach tag must specify a dir attribute:\n\n{}".format(ET.tostring(e))
+            raise ParseError(msg)
+
+        if len(att) != 2:
+            msg = ("foreach tag has unrecognized attribute:\n\n"
+                   "{}\n\nValid Attributes: 'id, dir'".format(ET.tostring(e).rstrip()))
+            raise ParseError(msg)
+
         self.id = att['id']
-        assert 'dir' in att, 'the foreach tag must specify a dir attribute'
         self.dir = att['dir']
+
         for child in e:
             t = child.tag
-            assert t in ForEach.validTags, 'invalid tag in ForEach:' + t
+            if t not in ForEach.validTags:
+                msg = ("Illegal tag in foreach '{}': \n\n"
+                       "{}\n\nValid Tags: '{}'".format(self.name,
+                                                       ET.tostring(child).rstrip(),
+                                                       ", ".join(ForEach.validTags)))
+                raise ParseError(msg)
             if t == 'step':
                 self.steps.append(child)
             elif t == 'file':
-                assert not self.file, 'the foreach tag must contain exactly one file tag.'
+                if self.file:
+                    msg = "The foreach tag must contain exactly one file tag:\n\n{}".format(ET.tostring(e))
+                    raise ParseError(msg)
                 self.file = ForEachFile(child, self.pipelineFiles)
             else:
                 ForEachRelated(child, self.relatedFiles, pipeline_files)
-        assert self.file, 'the foreach tag must contain a file tag'
-        assert self.file.id not in self.relatedFiles, "a foreach file's id must not be the same as a related file's id" + self.file.id
+
+        if not self.file:
+            msg = "The foreach tag must contain a file tag:\n\n{}".format(ET.tostring(e))
+            raise ParseError(msg)
+
+        if self.file.id in self.relatedFiles:
+            msg = ("A foreach file's id must not be the same as a related "
+                   "file's id.\n"
+                   "'{}' is used as a file and related file id in '{}'.\n\n{}")
+            raise ParseError(msg.format(self.file.id, self.id, ET.tostring(e)))
 
     def submit(self, name_prefix):
 
@@ -79,7 +109,7 @@ class ForEach():
             cleanups = []
             files_to_delete = []
             iteration_ids = []
-            #TODO this is difficult to make sense of, create a static method in
+            #TODO this is impossible to make sense of, create a static method in
             #PipelineFile that only takes the id, path, file list, and directory
             PipelineFile(self.file.id, fn, True, False, True, False,
                          self.pipelineFiles, True, False, None, None, None, None,
@@ -99,7 +129,7 @@ class ForEach():
                     #related file is an output file and indir not specified
                     #write it to the default output directory
                     directory = None
-                #TODO see comments for PipelineFile above
+                #TODO see comments for PipelineFile above. this is wicked ugly
                 PipelineFile(rel.id, rfn, True, False, rel.is_input, False,
                              self.pipelineFiles, True, False, None, None, None,
                              None, None, None, directory, False, False, None)
@@ -164,7 +194,7 @@ class ForEach():
         return missing
 
         
-class ForEachFile():
+class ForEachFile(object):
     requiredAtts = [
         'id',
         'pattern' ]
@@ -172,15 +202,25 @@ class ForEachFile():
     def __init__(self, e, pipeline_files):
         atts = e.attrib
         for a in atts:
-            assert a in ForEachFile.requiredAtts, 'foreach file tag has unrecognized attribute: ' + a
+            if a not in ForEachFile.requiredAtts:
+                msg = ("Illegal attribute in foreach file: {}\n\n"
+                       "{}\n\nValid Attributes: '{}'".format(a, ET.tostring(e).rstrip(),
+                                                             ", ".join(ForEachFile.requiredAtts)))
+                raise ParseError(msg)
         for a in ForEachFile.requiredAtts:
-            assert a in atts, 'foreach file tag is missing required attribute: ' + a
+            if a not in atts:
+                msg = ("foreach file tag missing required attribute:\n\n{}\n\n"
+                       "Required Attributes: '{}'".format(ET.tostring(e).rstrip(),
+                                                          ", ".join(ForEachFile.requiredAtts)))
+                raise ParseError(msg)
         self.id = atts['id']
-        assert self.id not in pipeline_files, "a foreach file's id must not be the same as a pipeline file id: " + self.id
+        if self.id in pipeline_files:
+            msg = "a foreach file's id must not be the same as a pipeline file id: {}\n\n{}".format(self.id, ET.tostring(e))
+            raise ParseError(msg)
         self.pattern = re.compile(atts['pattern'])
         
 
-class ForEachRelated():
+class ForEachRelated(object):
     requiredAtts = [
         'id',
         'input',
@@ -194,12 +234,27 @@ class ForEachRelated():
     def __init__(self, e, related_files, pipeline_files):
         atts = e.attrib
         for a in atts:
-            assert a in ForEachRelated.requiredAtts or a in ForEachRelated.optionalAtts, 'foreach related tag has unrecognized attribute: ' + a
+            if a not in ForEachRelated.requiredAtts and a not in ForEachRelated.optionalAtts:
+                msg = ("foreach related tag has unrecognized attribute: {}\n\n"
+                       "{}\n\nValid Attributes: '{}'".format(a, ET.tostring(e).rstrip(),
+                                                             ", ".join(ForEachRelated.requiredAtts + ForEachRelated.optionalAtts)))
+                raise ParseError(msg)
         for a in ForEachRelated.requiredAtts:
-            assert a in atts, 'foreach related tag is missing required attribute: ' + a
+            if a not in atts:
+                msg = ("foreach related tag missing required attribute:\n\n{}\n\n"
+                        "Required Attributes: '{}'".format(ET.tostring(e).rstrip(),
+                                                           ", ".join(ForEachRelated.requiredAtts)))
+                raise ParseError(msg)
+
         self.id = atts['id']
-        assert self.id not in related_files, 'foreach tag contains duplicate related file id: ' + self.id
-        assert self.id not in pipeline_files, "a foreach file's id must not be the same as a pipeline file id: " + self.id
+
+        if self.id in related_files:
+            msg = ("foreach tag contains duplicate related file id: {}\n\n{}".format(self.id, ET.tostring(e)))
+            raise ParseError(msg)
+        if self.id in pipeline_files:
+            msg = "a foreach related file's id must not be the same as a pipeline file id: {}\n\n{}".format(self.id, ET.tostring(e))
+            raise ParseError(msg)
+
         self.is_input = atts['input'].upper() == 'TRUE'
         self.pattern = re.compile(atts['pattern'])
         self.replace = atts['replace']
