3830cdf12def923a61dbad1a0071c38a5f1aba93
Glen Beane <glen.beane@jax.org>
2016-02-24 13:19:23 -0500

    fix stray quotes in elapsed_time_ranges.py, add LICENSE file to repo

--- bin/maintenance_programs/elapsed_time_ranges.py
+++ bin/maintenance_programs/elapsed_time_ranges.py
@@ -14,8 +14,9 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.ith this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 
 # Given a directory containing civet pipeline runs, report the
 # total elapsed time of successful runs.
